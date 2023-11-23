241d073a2697c29dfbe945773925d07197f0b6ed
Paz777 <111736251+Paz777@users.noreply.github.com>
2023-11-23 09:22:38 +0000

    add movie details - getting inputs/ simple validation

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -11,38 +11,58 @@ const startAddMovieButton = document.querySelector('header button');
 
 const background = document.getElementById('backdrop')
 const cancelAddMovieButton = addMovieModal.querySelector('.btn--passive')
+const addMovieDetails = cancelAddMovieButton.nextElementSibling;
+
+const userInputs = document.querySelectorAll('input');
+// other way to access all inputs
+//const userInputs2 = document.getElementsByTagName('input');
+
 
 const toggleMovieModal = () => {
 
-// 2 other ways that you cna toggle the visbiility
-//   if (addMovieModal.className === 'modal visible') {
-//     addMovieModal.className = 'modal';
-//   } else {
-//     addMovieModal.className = 'modal visible';
-//   }
+    // 2 other ways that you cna toggle the visbiility
+    //   if (addMovieModal.className === 'modal visible') {
+    //     addMovieModal.className = 'modal';
+    //   } else {
+    //     addMovieModal.className = 'modal visible';
+    //   }
 
-//   if (addMovieModal.classList.contains('visible')) {
-//     addMovieModal.classList.remove('visible');
-//   } else {
-//     addMovieModal.classList.add('visible');
-//   }
+    //   if (addMovieModal.classList.contains('visible')) {
+    //     addMovieModal.classList.remove('visible');
+    //   } else {
+    //     addMovieModal.classList.add('visible');
+    //   }
 
-  addMovieModal.classList.toggle('visible');
-  toggleBackground();
+    addMovieModal.classList.toggle('visible');
+    toggleBackground();
 };
 
 const toggleBackground = () => {
     background.classList.toggle('visible');
 };
 
-const cancelAddMovie = () => {
+const cancelAddMovieHandler = () => {
     toggleMovieModal();
-}
+};
 
 const backgroundClickHandler = () => {
     toggleMovieModal();
-}
+};
+
+const addMovieHandler = () => {
+    const title = userInputs[0].value;
+    const imageUrl = userInputs[1].value;
+    const rating = userInputs[2].value;
+
+    if (title.trim() === '' || imageUrl.trim() === '' || rating.trim() === '' || +rating < 1 || +rating > 5) {
+        alert("Enter valid movie information");
+        return;
+    }
+
+
+};
 
 startAddMovieButton.addEventListener('click', toggleMovieModal);
 background.addEventListener('click', backgroundClickHandler);
-cancelAddMovieButton.addEventListener('click', cancelAddMovie);
+cancelAddMovieButton.addEventListener('click', cancelAddMovieHandler);
+addMovieDetails.addEventListener('click', addMovieHandler);
